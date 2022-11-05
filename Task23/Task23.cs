6529c93a1e5158dd516932e3d7075fe2ca495c0c
Alexander <aromanov999@list.ru>
2022-11-05 16:52:07 +0400

    third commit

---
+++ Task23/Task23.cs
@@ -0,0 +1,24 @@
+﻿int N = 0;
+int number = GetNumber(N);
+PrintCubes(number);
+
+int GetNumber(int number)
+{
+    Console.WriteLine("Введите число N");
+    number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+void PrintCubes(int N)
+{
+    for (int i = 1; i <= N; i++)
+    {
+        if (i == N) {
+            Console.Write(i*i*i);
+
+        }
+        else {
+            Console.Write(i*i*i+", ");         
+        }    
+    }
+}
